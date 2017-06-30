ff53333eb0ea30ce464c24e09eb94a9eabf35551
Atish Narlawar <atish@atish.info>
2017-06-30 17:20:06 -0400

    Added Logs

--- mic.js
+++ mic.js
@@ -10,13 +10,13 @@ const models = new Models();
 
 var polly = new AWS.Polly({'region': 'us-east-1'});
 
-var recorder = require('./record');
+var recorder = require('./lib/record');
 var file = fs.createWriteStream('tempcommand.wav', { encoding: 'binary' });
 var detector = null;
 var apiai = require('apiai');
 
 const uuidv4 = require('uuid/v4');
-const SESSION_ID = uuidv4(); 
+const SESSION_ID = uuidv4();
 
 var apiAiApp = apiai("97a8b329b7f54fa7a7c786265d9a0027");
 var tempFileName = 'tempcommand.wav'
@@ -34,11 +34,11 @@ models.add({
   hotwords : 'smart mirror'
 });
 
-// models.add({
-//   file: 'resources/Ravina.pmdl',
-//   sensitivity: '0.5',
-//   hotwords : 'Ravina'
-// });
+models.add({
+  file: 'resources/Ravina.pmdl',
+  sensitivity: '0.5',
+  hotwords : 'Ravina'
+});
 
 
 //DFA states
@@ -51,19 +51,20 @@ var listenForHotword = function() {
 	});
 
 	detector.on('silence', function () {
-		console.log("silence");	
+		console.log("silence");
 	});
 
-	detector.on('sound', function (buffer) { // Buffer arguments contains sound that triggered the event, for example, it could be written to a wav stream 
+	detector.on('sound', function (buffer) { // Buffer arguments contains sound that triggered the event, for example, it could be written to a wav stream
 		console.log("not hotword");
 	});
 
 	detector.on('error', function () {
-	  console.log('error');
+
+	  console.log('Error on detector');
 	});
 
 	//add detection event
-	detector.on('hotword', function (index, hotword, buffer) { // Buffer arguments contains sound that triggered the event, for example, it could be written to a wav stream 
+	detector.on('hotword', function (index, hotword, buffer) { // Buffer arguments contains sound that triggered the event, for example, it could be written to a wav stream
   		console.log('hotword', index, hotword);
 		voiceTriggered = true;
 		recorder.stop();
@@ -95,20 +96,22 @@ var startRecordingCommand = function() {
 		sendAudioForProcessing()
 	}).pipe(file)
 
-	// Stop recording after three seconds 
+	// Stop recording after three seconds
 	setTimeout(function () {
-	  recorder.stop()
+	  console.log("Stop recording after three seconds");
+	  //recorder.stop();
+	 // file.end();
 	}, 3000)
 };
 
 var sendAudioForProcessing = function() {
-	// Detect the speech in an audio file. 
+	// Detect the speech in an audio file.
 	speechClient.recognize('./'+tempFileName, {
   		encoding: 'LINEAR16',
   		sampleRateHertz: 44100,
   		languageCode: "en_US"
 	}, function(err, transcript) {
-  		// transcript = 'how old is the Brooklyn Bridge' 
+  		// transcript = 'how old is the Brooklyn Bridge'
   		if (err) {
   			console.log("Error during google speech "+ err);
   		} else {
@@ -124,42 +127,60 @@ var sendTextForProcessing = function(text) {
     	sessionId: SESSION_ID
 	});
 	request.on('response', function(response) {
-	    console.log("Response from API.ai -- " + response.result.fulfillment.speech);
-	    convertTextToVoice(response.result.fulfillment.speech);
+		if(response && response.result && response.result.fulfillment){
+		    console.log("Response from API.ai -- " + response.result.fulfillment.speech);
+		    convertTextToVoice(response.result.fulfillment.speech);
+		}
 	});
 	request.on('error', function(error) {
     	console.log("Error during API.ai" + error);
 	});
-	request.end();	
+	request.end();
 	}
-	
 };
 
 
 var convertTextToVoice = function(text){
 	let params = {
-	  OutputFormat: "pcm", 
-	  Text: text, 
+	  OutputFormat: "pcm",
+	  Text: text,
 	  VoiceId: "Raveena"
 	 };
-  
+
    console.log("Feeding to Polly");
     polly.synthesizeSpeech(params, (err, data) => {
     if (err) {
        console.log("Error during polly");
         console.log(err.code)
     } else if (data) {
+
+		// var AudioStream = new Stream.Readable()
+		// AudioStream._read = function () {}
+		// AudioStream.pipe(new Speaker({
+		// 	  channels: 1,
+		// 	  bitDepth: 16,
+		// 	  sampleRate: 16000
+		// 	}))
+
+		// if (data.AudioStream instanceof Buffer) {
+		//   AudioStream.push(data.AudioStream)
+		// }
+  //              console.log("Finished the Polly");
+
+
         if (data.AudioStream instanceof Buffer) {
             // Initiate the source
             var bufferStream = new Stream.PassThrough()
             // convert AudioStream into a readable stream
             bufferStream.end(data.AudioStream)
             // Pipe into Player
+
             bufferStream.pipe(new Speaker({
 			  channels: 1,
 			  bitDepth: 16,
 			  sampleRate: 16000
 			}))
+               console.log("Finished the Polly");
         }
     }
 })
